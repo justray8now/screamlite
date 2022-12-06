35882a9f19cb4863642d6c64c3fc62460c64d8b2
R <91546971+justray8now@users.noreply.github.com>
2022-12-06 13:10:12 +0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,24 @@
+countries = ['Australia',
+                                                              'India ',
+                                                              'Indonesia',
+                                                              'Malaysia',
+                                                              'Pakistan',
+                                                              'Philippines',
+                                                              'Singapore',
+                                                              'Vietnam',
+                                                              'Turkey',
+                                                              'Thailand',
+                                                              'China',
+                                                              'Taiwan',
+                                                              'Hong Kong',
+                                                              'Japan']
+languages = ['ID','EN']
+topics = ['GENERAL', 'WORLD', 'NATION', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SPORTS',
+                                             'SCIENCE', 'HEALTH']
+
+AVAILABLE_COUNTRIES = {'Australia': 'AU', 'India ': 'IN',
+ 'Indonesia': 'ID', 'Malaysia': 'MY', 'Pakistan': 'PK', 'Philippines': 'PH', 'Singapore': 'SG', 'United States': 'US', 'Thailand': 'TH', 'China': 'CN', 'Taiwan': 'TW', 'Hong Kong': 'HK', 'Japan': 'JP'}
+
+AVAILABLE_LANGUAGES = {'english': 'en', 'indonesian': 'id'}
+
+suffixes = ('.jpg','.jpeg','.png','.JPG','.JPEG','.PNG')
